e9cd1007f9369a2e20be5ad395cb5c20d85594ee
Mountains-and-rivers <sjdykp@163.com>
2019-10-07 00:58:46 +0800

    init

---
+++ middleware/config-default.js
@@ -0,0 +1,12 @@
+
+var config = {
+    dialect: 'mysql',
+    database: 'zshop',
+    username: 'root',
+    password: '123456',
+    host: 'localhost',
+    port: 3306,
+    rootpath: '/zshop'
+};
+
+module.exports = config;
